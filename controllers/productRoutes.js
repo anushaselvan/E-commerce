573c878e9d0d3ed8dc8e4a00d86ffb323e7f646c
rmnk-nz <rua.manukau20@gmail.com>
2021-11-29 21:24:33 +1100

    adding route to show one product

--- controllers/productRoutes.js
+++ controllers/productRoutes.js
@@ -1,27 +1,28 @@
 const router = require("express").Router();
 
-const products = [
-    {
-        product_title: "Hawaiki Logo - Muscle Tee - Black",
-        description: "Suns out, guns out with your Hawaiki logo muscle tee. Ideal top for your everday workouts or a stylish sleevless option for the warm summer days!",
-        price: "$50",
-        filename: "black-hawaiki-logo-2.jpg",
-    },
-    {
-        product_title: "Hawaiki Logo - Muscle Tee - Grey",
-        description: "Suns out, guns out with your Hawaiki logo muscle tee. Ideal top for your everday workouts or a stylish sleevless option for the warm summer days!",
-        price: "$50",
-        filename: "grey-hawaiki-logo.jpg"
-    }
-];
+// const products = [
+//     {
+//         product_title: "Hawaiki Logo - Muscle Tee - Black",
+//         product_id:"1",
+//         description: "Suns out, guns out with your Hawaiki logo muscle tee. Ideal top for your everday workouts or a stylish sleevless option for the warm summer days!",
+//         price: "$50",
+//         filename: "black-hawaiki-logo-2.jpg",
+//     },
+//     {
+//         product_title: "Hawaiki Logo - Muscle Tee - Grey",
+//         product_id: "2",
+//         description: "Suns out, guns out with your Hawaiki logo muscle tee. Ideal top for your everday workouts or a stylish sleevless option for the warm summer days!",
+//         filename: "grey-hawaiki-logo.jpg"
+//     }
+// ];
 
-router.get("/products", async (req, res) => {
-    res.render("products", {products});
-});
+// router.get("/products", async (req, res) => {
+//     res.render("products", {products});
+// });
 
-router.get("/products/:num", async (req, res) => {
-    return res.render("product", products[req.params.num - 1]);
-});
+// router.get("/products/:id", async (req, res) => {
+//     return res.render("product", products[req.params.num - 1]);
+// });
   
 module.exports = router;
   
