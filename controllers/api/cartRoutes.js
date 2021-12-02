f84736516bce6eeeec9fc7a23049aa25e8400262
anushaselvan <anushaselvan@gmail.com>
2021-12-02 21:57:59 +1100

    Cart routes added

--- controllers/api/cartRoutes.js
+++ controllers/api/cartRoutes.js
@@ -19,83 +19,83 @@ router.get("/", async (req, res) => {
     }
 })
 
-// router.post("/", withAuth, async (req, res) => {
-//     // we expect the body to have a cart id
-//     console.log(`cartRoutes post hit`, req.body);
-//     const productId = req.body
-//     const userId = req.session.user_id
-//     try {
+router.post("/", async (req, res) => {
+    // we expect the body to have a cart id
+    console.log(`cartRoutes post hit`, req.body);
+    const productId = req.body
+    const userId = req.session.user_id
+    try {
       
-//         const newCartItem = await Cart.create({
-//             userId,
-//             productId,  
-//         })
+        const newCartItem = await Cart.create({
+            userId,
+            productId,  
+        })
   
-//       console.log()
-//       res.render("products", { products });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+      console.log()
+      res.status(200).json(newCartItem);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-//   router.put("/", withAuth, async (req, res) => {
-//     // we expect the body to have a cart id
-//     console.log(`cartRoutes hit`, req.body);
-// const cardItemId = ""
-//     try {
-//         const updatedCartItem = await Cart.update({
-//             cardItemId
-//         }, {
-//             ...req.body
-//         })
+  router.put("/", async (req, res) => {
+    // we expect the body to have a cart id
+    console.log(`cartRoutes hit`, req.body);
+const cardItemId = ""
+    try {
+        const updatedCartItem = await Cart.update({
+            cardItemId
+        }, {
+            ...req.body
+        })
   
-//       console.log()
-//       res.render("products", { products });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+      console.log()
+      res.status(200).json(updatedCartItem);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-//   router.delete("/", withAuth, async (req, res) => {
-//     // we expect the body to have a cart id
-//     console.log(`cartRoutes hit`, req.body);
-//     const cardItemId = ""
-//     try {
+  router.delete("/", withAuth, async (req, res) => {
+    // we expect the body to have a cart id
+    console.log(`cartRoutes hit`, req.body);
+    const cardItemId = ""
+    try {
       
-//         const updatedCartItem = await Cart.destroy({
-//              cardItemId 
-//         })
+        const updatedCartItem = await Cart.destroy({
+             cardItemId 
+        })
   
-//       console.log()
-//       res.render("products", { products });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+      console.log()
+      res.render("products", { products });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-//   router.post("/checkout", async (req, res) => {
-//      try {
-//          const userId = ""
-//          // get the card items and then send and email
-//          const cardItems = await Cart.findAll({
-//             where: {
-//                 userId
-//             }
-//         })
+  router.post("/checkout", async (req, res) => {
+     try {
+         const userId = ""
+         // get the card items and then send and email
+         const cardItems = await Cart.findAll({
+            where: {
+                userId
+            }
+        })
 
-//         // TODO: send email
-//         // nodemailer // recommend
-//         // sendgrid 
+        // TODO: send email
+        // nodemailer // recommend
+        // sendgrid 
 
-//         const deletedCartItems = await Cart.destroy({
-//             where: {
-//                 userId
-//             }
-//         })
-//         res.json({message: "Your order has successfuly been placed!"})
-//      }catch( e) {
-//          res.status(500).json({message: "Something went wrong"})
-//      }
-//   })
+        const deletedCartItems = await Cart.destroy({
+            where: {
+                userId
+            }
+        })
+        res.json({message: "Your order has successfuly been placed!"})
+     }catch( e) {
+         res.status(500).json({message: "Something went wrong"})
+     }
+  })
 
 module.exports = router;
