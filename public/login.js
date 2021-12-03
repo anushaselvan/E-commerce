fef92412ffc04e5a71a8d63f7cc892e2271f8017
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-12-04 00:46:36 +1100

    user login through modal working

--- public/login.js
+++ public/login.js
@@ -1,19 +1,86 @@
 // const $ = require('jquery');
-window.onload = function(){
-$('#modal').iziModal();
+window.onload = function () {
+  $("#modal-custom").iziModal({
+    tranitionOut: 'comingOut'
+    // title: 'This is a test TITLE',
+    // timeout: 5000,
+    // timeoutProgressbar: true,
+    // timeoutProgressbarColor: 'rgba(255,255,255,0.5)',
+  });
+  $('#modal-alert').iziModal({
+    title: 'Welcome Back',
+    timeout: 1500,
+    timeoutProgressbar: true,
+    timeoutProgressbarColor: 'rgba(255,255,255,0.5)',
+    width: 300,
+    onClosing: function () {document.location.reload();}
+  })
+  // $(document).on('click', '#modal', function (event) {
+  //   event.preventDefault();
+  //   console.log('clicked');
+  //   // $('#modal').iziModal('setZindex', 99999);
+  //   // $('#modal').iziModal('open', { zindex: 99999 });
+  //   // $('#modal').iziModal('open'//, {
+  //   //    transitionIn: 'bounceInDown',
+  //   //    transitionOut: 'bounceOutDown' // TransitionOut will be applied if you have any open modal.
+  //   // }
+  //   // );
+  // });
 
-$(document).on('click', '#modal', function (event) {
-  event.preventDefault();
-  console.log(clicked);
-  // $('#modal').iziModal('setZindex', 99999);
-  // $('#modal').iziModal('open', { zindex: 99999 });
-  $('#modal').iziModal('open' //{
-    // transitionIn: 'bounceInDown',
-    // transitionOut: 'bounceOutDown' // TransitionOut will be applied if you have any open modal.
-  );
-});
+// 
 
-}
+  $("#modal-custom").on("click", "header a", function (event) {
+    event.preventDefault();
+    const index = $(this).index();
+    $(this).addClass("active").siblings("a").removeClass("active");
+    $(this)
+      .parents("div")
+      .find("section")
+      .eq(index)
+      .removeClass("hide")
+      .siblings("section")
+      .addClass("hide");
+
+    if ($(this).index() === 0) {
+      $("#modal-custom .iziModal-content .icon-close").css(
+        "background",
+        "#ddd"
+      );
+    } else {
+      $("#modal-custom .iziModal-content .icon-close").attr("style", "");
+    }
+  });
+
+  $("#modal-custom").on("submit", "#login-form", async function (event) {
+    event.preventDefault();
+    const email = document.querySelector("#login-email").value.trim();
+  const password = document.querySelector("#login-password").value.trim();
+
+  const response = await fetch("/api/users/login", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: { "Content-Type": "application/json" },
+  });
+console.log(response);
+  if (response.ok) {
+    $('#modal-custom').iziModal('close')
+    // document.location.reload();
+        $('#modal-alert').iziModal('open');
+    // alert(`logged in`);
+  } else {
+    // alert(`sorry not logged in`);
+    const fx = "wobble", //wobble shake
+      $modal = $(this).closest(".iziModal");
+    
+    if (!$modal.hasClass(fx)) {
+        $modal.addClass(fx);
+        setTimeout(function () {
+            $modal.removeClass(fx);
+          }, 1500);
+        }
+      }
+  });
+};
 /*
 
 
